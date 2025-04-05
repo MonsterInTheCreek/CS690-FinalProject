92856eec7225ea6270cd2d53333065c02c48dff1
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-04-05 17:56:41 -0400

    WIP non-stable: domain classes now work, but need to refine further

--- TasksApp/Tasks/Domain.cs
+++ TasksApp/Tasks/Domain.cs
@@ -23,5 +23,20 @@ public class TaskAction
 
 public class AppTask
 {
-    
+    public TaskAction TaskAction { get; set; }
+    public TaskTarget TaskTarget { get; set; }
+    public DateTime SchedDate { get; set; }
+    public int Frequency { get; set; }
+    public DateTime? CompDate { get; }   // null at instantiation, change value using function during write to Old log 
+    public DateTime? PrevDate { get; }   // set only at instantiation, but null is valid for first use
+
+    public AppTask(TaskAction taskAction, TaskTarget taskTarget, DateTime schedDate, int frequency, DateTime? compDate, DateTime? prevDate)
+    {
+        this.TaskAction = taskAction;
+        this.TaskTarget = taskTarget;
+        this.SchedDate = schedDate;
+        this.Frequency = frequency;
+        this.CompDate = compDate;
+        this.PrevDate = prevDate;
+    }
 }
--- TasksApp/Tasks/Program.cs
+++ TasksApp/Tasks/Program.cs
@@ -15,12 +15,14 @@ class Program
 
             do
             {
-                Task task = askForTask();
+                AppTask task = AskForTask();
                 File.AppendAllText("tasks-log.txt",
-                    task.taskAction + ";" +
-                    task.taskTarget + ";" +
-                    task.taskSchedDate + ";" +
-                    task.taskCompDate + ";" +
+                    task.TaskAction + ";" +
+                    task.TaskTarget + ";" +
+                    task.SchedDate + ";" +
+                    task.Frequency + ";" +
+                    // CompDate does not get stored
+                    task.PrevDate + ";" +
                     Environment.NewLine
                 );
 
@@ -39,46 +41,74 @@ class Program
         }
     }
 
-    public static Task askForTask()
+    public static AppTask AskForTask()
     {
-        string taskAction = RequestInput("What task action? ");
-        string taskTarget = RequestInput("To where? ");
-        string taskSchedDate = RequestInput("What day to schedule? (mm/dd/yy) ");
-        
-        string taskCompDate;
+        TaskAction taskAction = new TaskAction(RequestInput("What task action? "));
+        TaskTarget taskTarget = new TaskTarget(RequestInput("Where will you perfrom this? "));
+        DateTime schedDate = DateTime.Parse(RequestInput("What day to schedule? (mm/dd/yy) "));
+        int frequency = int.Parse(RequestInput("What frequency? (in days) "));
+        DateTime? compDate;
         string taskComplete = RequestInput("Have you completed this (yes or no) ");
         if (taskComplete == "yes")
         {
-            taskCompDate = RequestInput("When did you complete this task? (mm/dd/yy) ");
+            compDate = DateTime.Parse(RequestInput("When did you complete this task? (mm/dd/yy) "));
         } else {
-            taskCompDate = null;
+            compDate = null;
         }
-
-        Task task = new Task (taskAction, taskTarget, taskSchedDate, taskCompDate);
+        DateTime? prevDate = null;
+    
+        AppTask task = new AppTask (taskAction, taskTarget, schedDate, frequency, compDate, prevDate);
         return task;
     }
-
+    
     public static string RequestInput(string message)
     // need validations
     {
         Console.Write(message);
         return Console.ReadLine();
     }
-
-    public class Task
-    {
-        // remember that public references in fields is a bad idea, refactor
-        public string taskAction;
-        public string taskTarget;
-        public string taskSchedDate;
-        public string taskCompDate;
-        public Task (string taskAction, string taskTarget, string taskSchedDate, string taskCompDate)
-        {
-            this.taskAction = taskAction;
-            this.taskTarget = taskTarget;
-            this.taskSchedDate = taskSchedDate;
-            this.taskCompDate = taskCompDate;
-        }
-    }
+    
+    //
+    // public static Task askForTask()
+    // {
+    //     string taskAction = RequestInput("What task action? ");
+    //     string taskTarget = RequestInput("To where? ");
+    //     string taskSchedDate = RequestInput("What day to schedule? (mm/dd/yy) ");
+    //     
+    //     string taskCompDate;
+    //     string taskComplete = RequestInput("Have you completed this (yes or no) ");
+    //     if (taskComplete == "yes")
+    //     {
+    //         taskCompDate = RequestInput("When did you complete this task? (mm/dd/yy) ");
+    //     } else {
+    //         taskCompDate = null;
+    //     }
+    //
+    //     Task task = new Task (taskAction, taskTarget, taskSchedDate, taskCompDate);
+    //     return task;
+    // }
+    //
+    // public static string RequestInput(string message)
+    // // need validations
+    // {
+    //     Console.Write(message);
+    //     return Console.ReadLine();
+    // }
+    //
+    // public class Task
+    // {
+    //     // remember that public references in fields is a bad idea, refactor
+    //     public string taskAction;
+    //     public string taskTarget;
+    //     public string taskSchedDate;
+    //     public string taskCompDate;
+    //     public Task (string taskAction, string taskTarget, string taskSchedDate, string taskCompDate)
+    //     {
+    //         this.taskAction = taskAction;
+    //         this.taskTarget = taskTarget;
+    //         this.taskSchedDate = taskSchedDate;
+    //         this.taskCompDate = taskCompDate;
+    //     }
+    // }
 
 }

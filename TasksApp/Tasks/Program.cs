b7f227ce584fffe7875a13798c050f8a326169d5
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-04-10 21:24:43 -0400

    Stable: Adding & Completing Tasks now works.  Also added version status display at run.

--- TasksApp/Tasks/Program.cs
+++ TasksApp/Tasks/Program.cs
@@ -6,7 +6,25 @@ class Program
 {
     static void Main(string[] args)
     {
+        Version();
         AppUI appUI = new AppUI();
         appUI.Show();
+    }
+
+    static void Version()
+    {
+        Console.Clear();
+        Console.WriteLine("Version 1.0.0 - Alpha Release");
+        Console.WriteLine("Known current limitations:");
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("Target/Action - not used internally for new task creation");
+        Console.WriteLine("Target/Action - cannot remove items");
+        Console.WriteLine("UI is pretty ugly - need to upgrade via Spectre.Console");
+        Console.WriteLine("Low priority Supplies functionality not implemented at all");
+        Console.WriteLine("Code in general is kludgy, needs much refactoring");
+        Console.WriteLine("");
+        Console.WriteLine("...Press any key...");
+        Console.ReadKey(true);
+        Console.Clear();
     }
 }

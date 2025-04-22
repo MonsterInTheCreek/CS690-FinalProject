76ef5907817e15be2ae6ae2b4e7c1440e0fb6238
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-04-21 20:22:58 -0400

    Stable: Added some tests.

--- TasksApp/Tasks/Program.cs
+++ TasksApp/Tasks/Program.cs
@@ -12,11 +12,12 @@ class Program
     static void Version()
     {
         Console.Clear();
-        Console.WriteLine("Version pre-2.0.0 - Beta Release");
+        Console.WriteLine("Version 2.0.0 - Beta Release");
         Console.WriteLine("Known current limitations:");
         Console.WriteLine("--------------------------");
         Console.WriteLine("All Use Cases and Functional Requirements met");
         Console.WriteLine("Some kludge remains, but pragmatically project is successful.");
+        Console.WriteLine("I'll save you some trouble - my unit tests are lacking.  I recognize that.");
         Helpers.Wait();
     }
 }
